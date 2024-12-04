fd1bcfdc2f9874704cd15f1bd39a03e8c9562769
Connor Johnson <117045038+cmjspacegeek@users.noreply.github.com>
2024-12-04 10:19:55 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    BST tree = new BST();
+
+    tree.add(4);
+    tree.add(2);
+    tree.add(6);
+    tree.add(1);
+    tree.add(3);
+    tree.add(5);
+    tree.add(7);
+
+    System.out.println(tree.countLeafNodes());
     }
 }
