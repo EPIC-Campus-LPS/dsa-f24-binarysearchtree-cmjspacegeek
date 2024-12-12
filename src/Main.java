faa4c8efddfa3007743644a24622feb6de95c9d7
Connor Johnson <117045038+cmjspacegeek@users.noreply.github.com>
2024-12-12 11:02:44 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,21 @@ public class Main {
 
     tree.add(4);
     tree.add(2);
-    tree.add(6);
     tree.add(1);
     tree.add(3);
+    tree.add(6);
     tree.add(5);
     tree.add(7);
-
+    System.out.println(tree.height());
+    System.out.println(tree.countNodes());
     System.out.println(tree.countLeafNodes());
+    System.out.println(tree.contains(3));
+    System.out.println(tree.contains(10));
+    System.out.println(tree.preorder());
+    System.out.println(tree.postorder());
+    System.out.println(tree.inorder());
+    tree.delete(2);
+
+
     }
 }
